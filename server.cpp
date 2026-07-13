d196f0b659078e41d04f9ba10e935e25cb886d7d
jahidul39306 <jahidulislamananto@gmail.com>
2026-07-13 12:19:51 +0200

    listening and accepting new connections

--- server.cpp
+++ server.cpp
@@ -3,11 +3,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
+#include <unistd.h>
+#include <string.h>
 
 static void die(const char *msg){
     int err = errno;
     fprintf(stderr, "[%d] %s\n", err, msg);
     abort();
+}
+
+static void msg(const char *msg) {
+    fprintf(stderr, "%s\n", msg);
+}
+
+static void do_something(int connfd) {
+    char rbuf[64] = {};
+    ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
+    if (n < 0) {
+        msg("read() error");
+        return;
+    }
+    fprintf(stderr, "client says: %s\n", rbuf);
+
+    char wbuf[] = "world";
+    write(connfd, wbuf, strlen(wbuf));
 }
 
 int main(){
@@ -27,4 +46,20 @@ int main(){
     addr.sin_addr.s_addr = htonl(0);
     int rv = bind(fd, (const struct sockaddr *)&addr, sizeof(addr));
     if (rv) { die("bind()"); }
+
+    // listen
+    rv = listen(fd, SOMAXCONN); // for linux SOMAXCONN is 4096
+    if (rv) { die("listen()"); }
+
+    // keep accepting new connections
+    while (true) {
+        struct sockaddr_in client_addr = {};
+        socklen_t addrlen = sizeof(client_addr);
+        int connfd = accept(fd, (struct sockaddr *)&client_addr, &addrlen);
+        
+        if (connfd < 0) { continue; }
+
+        do_something(connfd);
+        close(connfd);
+    }
 }
