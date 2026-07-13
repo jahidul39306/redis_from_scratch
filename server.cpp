5eba593768512c0f91e84f6cdf91a27a4d962cd6
jahidul39306 <jahidulislamananto@gmail.com>
2026-07-13 11:29:37 +0200

    bind to an address

--- server.cpp
+++ server.cpp
@@ -1,8 +1,30 @@
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+
+static void die(const char *msg){
+    int err = errno;
+    fprintf(stderr, "[%d] %s\n", err, msg);
+    abort();
+}
 
 int main(){
+    // creating the fd
     int fd = socket(AF_INET, SOCK_STREAM, 0);
 
+    if (fd < 0) { die("socket()"); }
+
+    // setting sock option to reuse the same port 
     int val = 1;
     setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+
+    // bind to an address
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(1234);
+    addr.sin_addr.s_addr = htonl(0);
+    int rv = bind(fd, (const struct sockaddr *)&addr, sizeof(addr));
+    if (rv) { die("bind()"); }
 }
