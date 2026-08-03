0f2fb2eb10c54b766e61d5eba773d779e9287bf4
jahidul39306 <jahidulislamananto@gmail.com>
2026-08-03 09:36:55 +0200

    command req byte to string

--- server.cpp
+++ server.cpp
@@ -10,8 +10,11 @@
 #include <poll.h>
 
 #include <vector>
+#include <string>
+
 
 const size_t k_max_msg = 32 << 20;
+const size_t k_max_args = 3;
 
 struct Buffer {
     uint8_t *buffer_begin;
@@ -102,6 +105,50 @@ static void buf_consume(Buffer *buf, size_t n) {
         buf->data_begin = buf->buffer_begin;
         buf->data_end   = buf->buffer_begin;
     }
+}
+
+static bool read_u32(const uint8_t *&cur, const uint8_t *end, uint32_t &out) {
+    if (cur + 4 > end) {
+        return false;
+    }
+    memcpy(&out, cur, 4);
+    cur += 4;
+    return true;
+}
+
+static bool read_str(const uint8_t *&cur, const uint8_t *end, size_t n, std::string &out) {
+    if (cur + n > end) {
+        return false;
+    }
+    out.assign(cur, cur + n);
+    cur += n;
+    return true;
+}
+
+static int32_t parse_req(const uint8_t *data, size_t size, std::vector<std::string> &out) {
+    const uint8_t *end = data + size;
+    uint32_t nstr = 0;
+    if (!read_u32(data, end, nstr)) {
+        return -1;
+    }
+    if (nstr > k_max_args) {
+        return -1;  // limit to 3 arguments
+    } 
+
+    while (out.size() < nstr) {
+        uint32_t len = 0;
+        if (!read_u32(data, end, len)) {
+            return -1;
+        }
+        out.push_back(std::string());
+        if (!read_str(data, end, len, out.back())) {
+            return -1;
+        }
+    }
+    if (data != end) {
+        return -1;
+    }
+    return 0;
 }
 
 struct Conn {
